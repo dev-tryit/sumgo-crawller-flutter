30e5c11aeed121b4e433289cd76aab51f5c53bff
donghyunKim <imkim1893@naver.com>
2022-01-11 14:13:23 +0000

    apply static files

--- _code/lib/page/MainPage.dart
+++ _code/lib/page/MainPage.dart
@@ -7,34 +7,60 @@ import 'package:kdh_homepage/util/LogUtil.dart';
 import 'package:kdh_homepage/util/MediaQueryUtil.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
+  double containerWidth = 1024;
 
   @override
   Widget build(BuildContext context) {
-    LogUtil.info("${MediaQueryUtil.getScreenSize(context)}");
+    double width = MediaQueryUtil.getScreenSize(context).width;
+    LogUtil.info("$width");
 
     return SafeArea(
       child: Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: screen(width),
+      ),
+    );
+  }
+
+  Widget screen(double width) {
+    if (width > containerWidth) {
+      return desktop();
+    }
+    return mobile();
+  }
+
+  Widget desktop() {
+    return Center(
+      child: Container(
+        width: containerWidth,
+        color: Colors.white,
+        child: Row(
           children: [
-            const Spacer(flex: 43),
-            AppComponents.text(
-              fontSize: 52,
-              text: "${Setting.appBuildNumber}, Fullstack Developer, 김동현입니다.",
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(flex: 43),
+                AppComponents.text(
+                  fontSize: 52,
+                  text:
+                      "${Setting.appBuildNumber}, Fullstack Developer, 김동현입니다.",
+                ),
+                const Spacer(flex: 14),
+                AppComponents.text(
+                  fontSize: 39,
+                  text: "플루터 웹, 앱을 제작합니다. 스타트업의 시작을 도와드리겠습니다.",
+                ),
+                const Spacer(flex: 73),
+                const Placeholder(strokeWidth: 1, fallbackHeight: 426),
+                const Spacer(flex: 35),
+              ],
             ),
-            const Spacer(flex: 14),
-            AppComponents.text(
-              fontSize: 39,
-              text: "플루터 웹, 앱을 제작합니다. 스타트업의 시작을 도와드리겠습니다.",
-            ),
-            const Spacer(flex: 73),
-            const Placeholder(strokeWidth: 1, fallbackHeight: 426),
-            const Spacer(flex: 35),
           ],
         ),
       ),
     );
+  }
+
+  Widget mobile() {
+    return desktop();
   }
 }
