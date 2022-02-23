058ce93ab6392a36b78b435a83cffe76021712ef
donghyun <imkim1893@naver.com>
2022-02-23 08:47:51 +0000

    이미지 추가

--- _code/lib/page/MainPage.dart
+++ _code/lib/page/MainPage.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:kdh_homepage/util/MyImage.dart';
+
 class MainPage extends StatelessWidget {
-  const MainPage({ Key? key }) : super(key: key);
+  const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: MyImage.blueBackgroundImage,
+      ),
     );
   }
 }
