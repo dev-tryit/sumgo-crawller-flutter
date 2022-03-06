72add425213361459c5b5a7aaa52b44925b0780f
KimDonghyun <imkim189371@gmail.com>
2022-03-06 12:09:44 +0900

    copyAppSo.sh 수정 완료

--- _code/scripts/copyAppSo.sh
+++ _code/scripts/copyAppSo.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+p_path='C:/Users/imkim/StudioProjects/sumgo-crawller-flutter/_code'
+echo "$p_path"
+cd $p_path
+
+echo "flutter build windows"
+flutter build windows
+
+echo "$p_path/scripts"
+cd $p_path/scripts
+
 echo "cp -r ../build/windows/app.so ../build/windows/runner/Debug/data"
 cp -r ../build/windows/app.so ../build/windows/runner/Debug/data
