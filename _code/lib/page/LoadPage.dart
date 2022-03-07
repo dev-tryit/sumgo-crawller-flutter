fdc76f45aa70fc76c7e7a642c771bf0593e6cad1
KimDonghyun <imkim189371@gmail.com>
2022-03-07 20:43:33 +0900

    로그인 페이지 만드는중

--- _code/lib/page/LoadPage.dart
+++ _code/lib/page/LoadPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:kdh_homepage/_common/abstract/KDHStatelessWidget.dart';
+import 'package:kdh_homepage/_common/abstract/KDHState.dart';
+import 'package:kdh_homepage/_common/model/WidgetToGetSize.dart';
 import 'package:kdh_homepage/_common/util/PageUtil.dart';
 import 'package:kdh_homepage/_common/util/PlatformUtil.dart';
 import 'package:kdh_homepage/page/auth/AuthPage.dart';
@@ -8,12 +9,27 @@ import 'package:kdh_homepage/page/main/MainLayout.dart';
 import 'package:kdh_homepage/util/MyAuthUtil.dart';
 import 'package:kdh_homepage/util/MyColors.dart';
 
-class LoadPage extends KDHStatelessWidget {
+class LoadPage extends StatefulWidget {
+  const LoadPage({Key? key}) : super(key: key);
+
+  @override
+  _LoadPageState createState() => _LoadPageState();
+}
+
+class _LoadPageState extends KDHState<LoadPage> {
   LoadPageComponent c = LoadPageComponent();
 
   @override
-  Widget widgetToBuild(BuildContext context) {
-    return Scaffold(body: PlatformUtil.isMobile() ? mobile() : desktop());
+  bool isPage() => true;
+
+  @override
+  List<WidgetToGetSize> makeWidgetListToGetSize() => [];
+
+  @override
+  void mustRebuild() {
+    widgetToBuild =
+        () => Scaffold(body: PlatformUtil.isMobile() ? mobile() : desktop());
+    rebuild();
   }
 
   Widget desktop() {
@@ -25,11 +41,14 @@ class LoadPage extends KDHStatelessWidget {
   }
 
   @override
-  Future<void> afterBuild(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 3));
+  Future<void> afterBuild() async {
+    await Future.delayed(const Duration(seconds: 1));
     PageUtil.movePage(
         context, await MyAuthUtil.isLogin() ? MainLayout() : AuthPage());
   }
+
+  @override
+  Future<void> onLoad() async {}
 }
 
 class LoadPageComponent {
--- _code/lib/page/auth/AuthPage.dart
+++ _code/lib/page/auth/AuthPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kdh_homepage/util/MyColors.dart';
 
 class AuthPage extends StatefulWidget {
   @override
@@ -8,6 +10,38 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(child: Text("로그인")));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 36),
+            Text(
+              "숨고 매니저",
+              style: GoogleFonts.blackHanSans(
+                fontSize: 35,
+                color: MyColors.deepBlue,
+              ),
+            ),
+            inputBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget inputBox() {
+    return Column(
+      children: [
+        Text("휴대 전화번호"),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(),
+            ),
+            Text("인증 요청"),
+          ],
+        ),
+      ],
+    );
   }
 }
