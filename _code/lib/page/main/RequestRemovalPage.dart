19a5d1220705afdbb80117f987e6e7ce0c6cef45
TryIt <imkim1893@naver.com>
2022-03-24 22:18:07 +0900

    RequestRemovalPage 완성

--- _code/lib/page/main/RequestRemovalPage.dart
+++ _code/lib/page/main/RequestRemovalPage.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sumgo_crawller_flutter/_common/abstract/KDHComponent.dart';
+import 'package:sumgo_crawller_flutter/_common/abstract/KDHService.dart';
+import 'package:sumgo_crawller_flutter/_common/abstract/KDHState.dart';
+import 'package:sumgo_crawller_flutter/_common/model/WidgetToGetSize.dart';
+import 'package:sumgo_crawller_flutter/_common/util/AnimationUtil.dart';
+import 'package:sumgo_crawller_flutter/_common/util/LogUtil.dart';
+import 'package:sumgo_crawller_flutter/_common/util/PageUtil.dart';
+import 'package:sumgo_crawller_flutter/_common/util/PlatformUtil.dart';
+import 'package:sumgo_crawller_flutter/repository/AnalysisItemRepository.dart';
+import 'package:sumgo_crawller_flutter/repository/RemovalConditionRepository.dart';
+import 'package:sumgo_crawller_flutter/util/MyColors.dart';
 import 'package:sumgo_crawller_flutter/util/MyComponents.dart';
 import 'package:sumgo_crawller_flutter/util/MyCrawller.dart';
+import 'package:sumgo_crawller_flutter/util/MyFonts.dart';
 import 'package:sumgo_crawller_flutter/util/MyImage.dart';
 import 'package:sumgo_crawller_flutter/widget/MyCard.dart';
+import 'package:sumgo_crawller_flutter/widget/MyChart.dart';
 import 'package:sumgo_crawller_flutter/widget/MyRedButton.dart';
 import 'package:sumgo_crawller_flutter/widget/MyWhiteButton.dart';
 
@@ -13,17 +32,42 @@ class RequestRemovalPage extends StatefulWidget {
   _RequestRemovalPageState createState() => _RequestRemovalPageState();
 }
 
-class _RequestRemovalPageState extends State<RequestRemovalPage> {
-  late final RequestRemovalPageService s;
+class _RequestRemovalPageState extends KDHState<RequestRemovalPage,
+    RequestRemovalPageComponent, RequestRemovalPageService> {
+  @override
+  bool isPage() => false;
 
   @override
-  void initState() {
-    super.initState();
-    s = RequestRemovalPageService(this);
+  makeComponent() => RequestRemovalPageComponent(this);
+
+  @override
+  makeService() => RequestRemovalPageService(this, c);
+
+  @override
+  List<WidgetToGetSize> makeWidgetListToGetSize() => [];
+
+  @override
+  Future<void> onLoad() async {
+    await s.resetRemovalConditionList();
   }
 
   @override
-  Widget build(BuildContext context) {
+  void mustRebuild() {
+    widgetToBuild = () => c.body(s);
+    rebuild();
+  }
+
+  @override
+  Future<void> afterBuild() async {}
+}
+
+class RequestRemovalPageComponent
+    extends KDHComponent<_RequestRemovalPageState> {
+  String errorMessage = "";
+
+  RequestRemovalPageComponent(_RequestRemovalPageState state) : super(state);
+
+  Widget body(RequestRemovalPageService s) {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
       child: Column(
@@ -31,7 +75,8 @@ class _RequestRemovalPageState extends State<RequestRemovalPage> {
           const SizedBox(height: 60),
           MyCard(
             title: "정리 조건",
-            rightButton: MyRedButton("생성하기", onPressed: () {}),
+            rightButton: MyRedButton("생성하기",
+                onPressed: () => showCreateItemBottomSheet(s)),
             contents: [
               cardListTile("[최우선키워드] Flutter"),
               cardListTile("[최우선키워드] Flutter"),
@@ -48,7 +93,7 @@ class _RequestRemovalPageState extends State<RequestRemovalPage> {
     );
   }
 
-  ListTile cardListTile(String title, {bool isPlusIcon=true}) {
+  ListTile cardListTile(String title, {bool isPlusIcon = true}) {
     return ListTile(
       leading: Padding(
         padding: EdgeInsets.only(top: 6),
@@ -61,14 +106,103 @@ class _RequestRemovalPageState extends State<RequestRemovalPage> {
       dense: true,
     );
   }
+
+  void showCreateItemBottomSheet(RequestRemovalPageService s) {
+    final TextEditingController titleController = TextEditingController();
+    final TextEditingController keywordController = TextEditingController();
+
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => StatefulBuilder(
+        builder: (bottomSheetContext, setState) => Container(
+          decoration: const BoxDecoration(
+            color: MyColors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 5, top: 5, bottom: 5),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('키워드 분류 생성하기',
+                        style: MyFonts.gothicA1(
+                            color: MyColors.black,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500)),
+                    Expanded(
+                      child: Text(
+                        errorMessage,
+                        textAlign: TextAlign.right,
+                        style: MyFonts.gothicA1(
+                          color: MyColors.red,
+                          fontSize: 9,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    MyRedButton(
+                      "생성",
+                      useShadow: false,
+                      onPressed: () => s.addRemovalCondition(
+                        titleController.text.trim(),
+                        keywordController.text.trim(),
+                        setState,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                dense: true,
+                minLeadingWidth: 100,
+                leading: Text("분류 이름",
+                    style: MyFonts.gothicA1(
+                        color: MyColors.black, fontSize: 12.5)),
+                title: TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(isDense: true),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                dense: true,
+                minLeadingWidth: 100,
+                leading: Text("분류 기준 텍스트",
+                    style: MyFonts.gothicA1(
+                        color: MyColors.black, fontSize: 12.5)),
+                title: TextField(
+                  controller: keywordController,
+                  decoration: const InputDecoration(isDense: true),
+                ),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class RequestRemovalPageService {
-  final _RequestRemovalPageState state;
+class RequestRemovalPageService
+    extends KDHService<_RequestRemovalPageState, RequestRemovalPageComponent> {
+  List<RemovalCondition> removalConditionList = [];
 
-  const RequestRemovalPageService(this.state);
-
-  BuildContext get context => state.context;
+  RequestRemovalPageService(
+      _RequestRemovalPageState state, RequestRemovalPageComponent c)
+      : super(state, c);
 
   Future<void> removeRequests() async {
     try {
@@ -78,4 +212,9 @@ class RequestRemovalPageService {
       await MyComponents.dismissLoadingDialog();
     }
   }
+
+  Future<void> addRemovalCondition(
+      String trim, String trim2, StateSetter setState) async {}
+
+  Future<void> resetRemovalConditionList() async {}
 }
